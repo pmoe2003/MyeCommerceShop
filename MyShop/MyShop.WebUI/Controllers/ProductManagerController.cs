5168de0ac37aded765464d153b0b7570598830c8
pmoe2003 <69215154+pmoe2003@users.noreply.github.com>
2020-08-07 12:06:49 -0500

    Product Manager Pages Build and tested

---
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.Core.Models;
+using MyShop.DataAccess.InMemory;
+
+namespace MyShop.WebUI.Controllers
+{
+
+    
+    public class ProductManagerController : Controller
+    {
+        //Instance of the repository
+        ProductRepository context;
+        //Constructor
+        public ProductManagerController()
+        {
+            context = new ProductRepository();
+        }
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            //Return a list of products
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        //Create Product Display a page with Product
+        public ActionResult Create()
+        {
+            Product product = new Product();
+            return View(product);
+        }
+
+        //Details to be posted in
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            else
+            {
+                //Insert to collection
+                context.Insert(product);
+                //save changes
+                context.Commit();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        //Log the product from the db
+        public ActionResult Edit(string Id)
+        {
+            Product product = context.Find(Id);
+            
+            if (product == null)
+                {
+                return HttpNotFound();
+                }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        //Takes in the product that we are updated
+        [HttpPost]
+        public ActionResult Edit(Product product, string Id)
+        {
+            Product productToEdit = context.Find(Id);
+           
+            if(productToEdit == null)
+                {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(product);
+                }
+                //if not valid - return after manually updating
+                productToEdit.Category = product.Category;
+                productToEdit.Description = product.Description;
+                productToEdit.Image = product.Image;
+                productToEdit.Name = product.Name;
+                productToEdit.Price = product.Price;
+
+                //save changes
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        //Load the product from the database
+        public ActionResult Delete(string Id)
+        {
+            Product productToDelete = context.Find(Id);
+
+            if (productToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productToDelete);
+            }
+        }
+
+        //Return from the db to delete
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            Product productToDelete = context.Find(Id);
+
+            if (productToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(Id);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
