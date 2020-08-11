0022cddfec1733ee4aa90036188bc6aee48cfa4b
pmoe2003 <69215154+pmoe2003@users.noreply.github.com>
2020-08-11 13:08:42 -0500

    EnitityFrameWork

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        //Create a constructor to capture connectionstring that the
+        //base class is expecting
+        public DataContext()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        //Create constructor to identity what model will be stored in the database
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
