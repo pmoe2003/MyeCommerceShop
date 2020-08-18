231bca786cbd737c415773833b1db9463c0fd8a3
pmoe2003 <69215154+pmoe2003@users.noreply.github.com>
2020-08-18 15:29:57 -0500

    Created Order Manager

--- MyShop/MyShop.Core/Contracts/IOrderService.cs
+++ MyShop/MyShop.Core/Contracts/IOrderService.cs
@@ -11,6 +11,10 @@ namespace MyShop.Core.Contracts
     public interface IOrderService
     {
         void CreateOrder(Order baseOrder, List<BasketItemViewModel> basketItems);
+        List<Order> GetOrderList();
+        Order GetOrder(string Id);
+        void UpdateOrder(Order updatedOrder);
+
 
     }
 }
